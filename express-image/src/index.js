6358f3c3bcda2141b87065e721195a1e0ae900f5
Delphine Scherler <delphine.scherler@heig-vd.ch>
2021-05-22 17:00:08 +0200

    first try generate flats

--- express-image/src/index.js
+++ express-image/src/index.js
@@ -1,4 +1,34 @@
 var Chance = require('chance');
 var chance = new Chance();
 
-console.log("Bonjour " + chance.name());
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send(generateFlats());	
+});
+
+app.listen(3000, function () {
+	console.log('Accepting HTTP requests on port 3000.');
+});
+
+function generateFlats() {
+	var numberOfFlats = chance.integer({
+		min: 0,
+		max: 12
+	});
+	console.log(numberOfFlats);
+	var flats = [];
+	
+	for (var i = 0; i < numberOfFlats; i++) {	
+		
+		flats.push({
+			street : chance.address(),
+			city : chance.city(),
+			rent : chance.integer({ min: 800, max: 2500 }),
+			nbPieces : chance.integer({ min: 2, max: 13 }) * 0.5
+		});
+	};
+	console.log(flats);
+	return flats;		
+}			
