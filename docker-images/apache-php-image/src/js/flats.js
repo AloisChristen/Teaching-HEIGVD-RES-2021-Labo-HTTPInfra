0fe8cb797b12a3136620e58c36357f712dd2e503
AloisChristen <alois.christen@heig-vd.ch>
2021-06-05 22:40:04 +0200

    Better index.html page, information about flats prettier

--- docker-images/apache-php-image/src/js/flats.js
+++ docker-images/apache-php-image/src/js/flats.js
@@ -6,9 +6,17 @@ $(function() {
                   console.log(flats);
                   var message = "Nothing here";
                   if ( flats.length > 0 ) {
-                          message = flats[0].street + ", " + flats[0].city;
-                  }
-                  $(".flats > h2").text(message);
+					  $(".flats > h2").attr("display", "none");
+					  $(".flats > table").removeAttr("display");
+					  $(".flats > table .flat-row-address").text(flats[0].street + ", " + flats[0].city);
+					  $(".flats > table .flat-row-rent").text(flats[0].rent + "chf");
+					  $(".flats > table .flat-row-rooms").text(flats[0].nbPieces); 
+                  } else {
+					  $(".flats > table").attr("display", "none");
+					  $(".flats > h2").removeAttr("display");
+					  $(".flats > h2").text(message);
+				  }
+                  
           });
   };
 
