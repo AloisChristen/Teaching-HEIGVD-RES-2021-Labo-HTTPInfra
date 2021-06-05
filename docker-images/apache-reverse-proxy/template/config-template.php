46d394807fd31a371d4d97ec0443aab80163de7c
AloisChristen <alois.christen@heig-vd.ch>
2021-06-05 12:17:56 +0200

    Modify 001-conf with php to use environnement variables

---
+++ docker-images/apache-reverse-proxy/template/config-template.php
@@ -0,0 +1,18 @@
+<?php
+	$static_app = getenv('STATIC_APP');
+	$dynamic_app = getenv('DYNAMIC_APP');
+?>
+
+<VirtualHost *:80>
+	ServerName demo.res.ch
+	
+	#ErroLog ${APACHE_LOG_DIR}/error.log
+	#CustomLog ${APACHE_LOG_DIR}/access.log combined
+	
+	ProxyPass '/api/flats/' 'http://<?php print "$dynamic_app"?>'
+	ProxyPassReverse '/api/flats/' 'http://<?php print "$dynamic_app"?>'
+	
+	ProxyPass '/' 'http://<?php print "$static_app"?>'
+	ProxyPassReverse '/' 'http://<?php print "$static_app"?>'
+
+</VirtualHost>
